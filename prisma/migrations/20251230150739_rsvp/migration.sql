21d7a0190c60ef7ded08628094eaacf5b90a7501
theoxav <theoxxav@outlook.fr>
2025-12-30 16:08:19 +0100

    Add RSVP model and enum for event participation tracking
    - Introduced RSVP model to manage user responses to events, including status and timestamps.
    - Added RSVPStatus enum to define possible RSVP states: GOING, NOT_GOING, and MAYBE.
    - Updated User and Event models to establish relationships with RSVP.
    - Created migration script for the new RSVP table and associated constraints.

---
+++ prisma/migrations/20251230150739_rsvp/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "RSVPStatus" AS ENUM ('GOING', 'NOT_GOING', 'MAYBE');
+
+-- CreateTable
+CREATE TABLE "RSVP" (
+    "id" TEXT NOT NULL,
+    "status" "RSVPStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+
+    CONSTRAINT "RSVP_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RSVP_userId_eventId_key" ON "RSVP"("userId", "eventId");
+
+-- AddForeignKey
+ALTER TABLE "RSVP" ADD CONSTRAINT "RSVP_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RSVP" ADD CONSTRAINT "RSVP_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
